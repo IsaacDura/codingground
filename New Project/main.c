aba93cbdec1dd96f28baebb66fbea184874a70fa
Isaac <isaacdura@hotmail.com>
2015-02-04 21:55:45 +0100

    Created by Coding Ground Client

---
+++ New Project/main.c
@@ -0,0 +1,45 @@
+/*Ejercicio 2 pag 15*/
+
+#include <stdio.h>
+
+int main(void){
+
+      float notasTotal;
+      int alumnos;
+      float notaAlta;
+      float notaBaja;
+      float nota1;
+      float nota2;
+      float notasMedia;
+      float mediaSuspendenSeguro;
+      float mediaNoPromedio;
+      float mediaAprueban;
+      float mediaDudoso;
+
+
+
+
+      printf("Introduce número alumnos\n");
+
+      scanf("%d",&alumnos);
+
+      printf("Introduce nota1\n");
+
+      scanf("%f",&nota1);
+
+      printf("Introduce nota2\n");
+
+      scanf("%f",&nota2);
+
+
+
+      printf("alumno nota 1: %d\n",nota1);
+      printf("alumno nota 2: %d\n",nota2);
+
+
+
+
+      return 0;
+
+       }
+
